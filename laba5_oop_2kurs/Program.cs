9e0afaeafa88232073885668f58d366ad75d0420
Kireev <Kireev@DESKTOP-PF4DRIV>
2021-10-26 17:14:36 +0300

    Add project files.

---
+++ laba5_oop_2kurs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace laba5_oop_2kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (float a = 1; a < 2; a = a + 0.25f)
+            {
+                Console.WriteLine($"A = {a}");
+                for (double x = 0; x <= 10; x += 0.25)
+                    Console.WriteLine($"X = {x}, y = {Calc(x, a)}");
+                Console.WriteLine("________________");
+            }
+            Console.ReadKey();
+        }
+
+        private static double Calc(double x, double a)
+        {
+            float e = 2.71828183f;
+            return a * Math.Pow(x, a) * Math.Pow(e, (-x / a));
+        }
+    }
+}
