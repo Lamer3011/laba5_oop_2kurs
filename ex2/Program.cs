1f468a384c2f33741a4598a197b851981f6defe8
Sergiy Kireev <48791896+Lamer3011@users.noreply.github.com>
2021-10-26 18:34:42 +0300

    Update Program.cs

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -6,7 +6,39 @@ namespace ex2
     {
         static void Main(string[] args)
         {
-            
+            double x, y, ch, zn, a,
+                S = 0, 
+                p = 1, 
+                d = 1;
+            int k = 0;
+            Console.Write("Enter x:");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Intermediate results");
+            a = x;
+
+            while (Math.Abs(a) >= 0.000001)
+            {
+                S += a;
+                //ch
+                ch = x * d;
+                d = x * d;
+                Console.WriteLine("Ch = " + ch);
+                //zn
+                zn = 2.0 * p;
+                p = 2.0 * p;
+                Console.WriteLine("Zn = " + zn);
+                a = ch / zn;
+                k++;
+                Console.WriteLine($"Iteration #{k}");
+                Console.WriteLine($"a = {a} \t S = {S}");
+            }
+            y = x / (2d - x);
+            Console.WriteLine("\n   Results:");
+            Console.WriteLine($"Given x = {x}");
+            Console.WriteLine($"The calculated sum of the series S = {S}");
+            Console.WriteLine($"Number of members of the row {k}");
+            Console.WriteLine($"Function x/(2-x) = " + y);
+            Console.ReadKey();
         }
     }
 }
